7d00a218f075a7f36ec26e4e4548a00923cf7197
Ahnaf Ahmed Oyon <ahmedahnaf513@gmail.com>
2026-05-03 21:26:29 +0600

    Add confusion matrix and classification report
    Implement confusion matrix visualization and report generation for test data.

---
+++ Confusion_Matrix.py
@@ -0,0 +1,15 @@
+y_pred = []
+y_true = []
+
+for images, labels in test_ds:
+    preds = model.predict(images)
+    y_pred.extend(np.argmax(preds, axis=1))
+    y_true.extend(np.argmax(labels.numpy(), axis=1))
+
+cm = confusion_matrix(y_true, y_pred)
+disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=class_names)
+disp.plot(xticks_rotation=45)
+plt.title("Confusion Matrix - Test Data")
+plt.show()
+
+print(classification_report(y_true, y_pred, target_names=class_names))
